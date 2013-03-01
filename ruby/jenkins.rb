bae7786473db44e8e9096ad1219b7dce5321ad4e
zm <zmzhuai@gmail.com>
2013-03-01 17:35:24 +0800

    ruby

---
+++ ruby/jenkins.rb
@@ -0,0 +1,23 @@
+cmd = "xcodebuild -target BrowseOverflowTests -sdk iphonesimulator clean build RUN_UNIT_TEST_WITH_IOS_SIM=YES > ./ruby/buildlog.txt 2> ./ruby/testlog.txt"
+system(cmd)
+
+result = ""
+File.open("./ruby/buildlog.txt", "r") do |f|
+	result = f.read
+end
+
+fails = result.scan /Test Case .* failed/
+passes = result.scan /Test Case .* passed/
+
+if fails.length>0
+	puts fails
+	raise "Unit Tests failed"
+else
+	if passes.length>0
+		puts "Unit Tests passed"
+	else
+		puts result
+		raise "Unit Test setup problem"
+	end
+end
+
